3dc9c6a36a6e1db3b82651c92e3bf0a89f98f8aa
Dmitriy Dudkin <dudkin.dv.dev@gmail.com>
2023-08-10 20:16:13 +0200

    Initial Commit

---
+++ ExhibitabilityParser/ExhibitabilityParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExhibitabilityParserApp.swift
+//  ExhibitabilityParser
+//
+//  Created by Дмитрий Дудкин on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExhibitabilityParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
