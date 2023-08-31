9a0c307f9c649202300d67a178e6396bf95d266c
Dmitriy Dudkin <dudkin.dv.dev@gmail.com>
2023-08-31 21:17:38 +0200

    constants

---
+++ ExhibitabilityParser/Helpers/String+Constants.swift
@@ -0,0 +1,13 @@
+//
+//  String+Constants.swift
+//  ExhibitabilityParser
+//
+//  Created by Дмитрий Дудкин on 31.08.2023.
+//
+
+import Foundation
+
+extension String {
+    static let rowSeparator: String = "\n"
+    static let columnSeparator: String = ";"
+}
